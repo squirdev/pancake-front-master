894f47329279d5018caca76519fcbfd9ab08e458
leondragan <hutsaloyevhen@gmail.com>
2022-10-21 07:47:18 +0400

    first commit

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useLocation } from 'react-router'
+import { Menu as UikitMenu } from '@pancakeswap/uikit'
+import { languageList } from 'config/localization/languages'
+import { useTranslation } from 'contexts/Localization'
+import useTheme from 'hooks/useTheme'
+import { usePriceCakeBusd } from 'state/farms/hooks'
+import { useProfile } from 'state/profile/hooks'
+import config from './config/config'
+import UserMenu from './UserMenu'
+import GlobalSettings from './GlobalSettings'
+import { getActiveMenuItem, getActiveSubMenuItem } from './utils'
+import { footerLinks } from './config/footerConfig'
+
+const Menu = (props) => {
+  const { isDark, toggleTheme } = useTheme()
+  const cakePriceUsd = usePriceCakeBusd()
+  const { profile } = useProfile()
+  const { currentLanguage, setLanguage, t } = useTranslation()
+  const { pathname } = useLocation()
+
+  const activeMenuItem = getActiveMenuItem({ menuConfig: config(t), pathname })
+  const activeSubMenuItem = getActiveSubMenuItem({ menuItem: activeMenuItem, pathname })
+
+  return (
+    <UikitMenu
+      userMenu={<UserMenu />}
+      globalMenu={<GlobalSettings />}
+      isDark={isDark}
+      toggleTheme={toggleTheme}
+      currentLang={currentLanguage.code}
+      langs={languageList}
+      setLang={setLanguage}
+      cakePriceUsd={cakePriceUsd.toNumber()}
+      links={config(t)}
+      subLinks={activeMenuItem?.hideSubNav ? [] : activeMenuItem?.items}
+      footerLinks={footerLinks(t)}
+      activeItem={activeMenuItem?.href}
+      activeSubItem={activeSubMenuItem?.href}
+      buyCakeLabel={t('Buy CAKE')}
+      profile={{
+        username: profile?.username,
+        image: profile?.nft ? `/images/nfts/${profile.nft?.images.sm}` : undefined,
+        profileLink: '/profile',
+        noProfileLink: '/profile',
+        showPip: !profile?.username,
+      }}
+      {...props}
+    />
+  )
+}
+
+export default Menu
